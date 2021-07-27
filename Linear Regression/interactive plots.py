02ea1fa7d593d92e369ca85d6240e52ce5068af9
Sidhved <sidhved.warik@gmail.com>
2021-07-28 00:10:59 +0530

    Linear Regression Part 1 Completed

---
+++ Linear Regression/interactive plots.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+X = pd.read_csv('D:\VS\Coding-Blocks-ML\Linear Regression\Linear_X_Train.csv').values
+Y = pd.read_csv('D:\VS\Coding-Blocks-ML\Linear Regression\Linear_Y_Train.csv').values
+
+theta = np.load("D:\VS\Coding-Blocks-ML\Linear Regression\ThetaList.npy")
+
+T0  = theta[:,0]
+T1 = theta[:,1]
+
+plt.ion()
+for i in range(0,50,3):
+    y_ = T1[i]*X + T0
+    #points
+    plt.scatter(X,Y)
+    # line 
+    plt.plot(X,y_,'red')
+    plt.draw()
+    plt.pause(1)
+    plt.clf()
